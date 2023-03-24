72de4f72f94ab448924edc4db791d9983f1646d5
abuzaud <antoine.buzaud@gmail.com>
2023-03-24 03:32:13 +0100

    ECLAIRE: feat(Auth) Ajout de l'authentification JWT. Comptes User en local

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,38 @@
-import { Controller, Get, Redirect, UseInterceptors } from '@nestjs/common';
-import { AppService } from './app.service';
-import { SentryInterceptor } from "./monitoring/sentry/sentry.interceptor";
+import {
+  Controller,
+  Get,
+  Post,
+  Redirect,
+  Request,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { SentryInterceptor } from './monitoring/sentry/sentry.interceptor';
+import { Public } from './auth/public.decorator';
+import { LocalAuthGuard } from './auth/local-auth-guard';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @UseInterceptors(SentryInterceptor)
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private authService: AuthService) {}
 
   @Get()
   @Redirect('api')
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   getHome(): void {}
+
+  @Public()
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req): Promise<object> {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return { email: req.user.email };
+  }
 }
---
+++ src/interfaces/user.interface.ts
@@ -0,0 +1,7 @@
+export interface UserInterface {
+  email: string;
+  firstname?: string;
+  lastname?: string;
+  password: string;
+  roles?: string[];
+}
