ed80971d6e73601a2221b5e576449c609450de58
Fabien Mercier <fabien.mercier@octo.com>
2023-07-25 10:59:19 +0200

    fix after code review

--- src/api/research-study/controllers/ResearchStudyQueryModel.ts
+++ src/api/research-study/controllers/ResearchStudyQueryModel.ts
@@ -1,21 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger'
 
 export class ResearchStudyQueryModel {
-  @ApiProperty({ description: 'ex : 2022-500014-26-00 (CTIS), 2021-A01861-40 (DM), 2021-000304-38 (JARDE)', required: false })
+  @ApiProperty({ description: 'e.g. CTIS : 2022-500014-26-00, e.g. DM : 2021-A01861-40, e.g. JARDE : 2021-000304-38', required: false })
     identifier: string
 
   @ApiProperty({ description: 'completed | approved | active | temporarily-closed-to-accrual | administratively-completed', required: false })
     status: string
 
-  @ApiProperty({ description: 'ex : (eq|ne|lt|le|gt)2019-10-18', required: false })
+  @ApiProperty({ description: 'e.g. : (eq|ne|lt|le|gt)2019-10-18', required: false })
     _lastUpdated: string
 
-  @ApiProperty({ description: 'ex : meta.lastUpdated', required: false })
+  @ApiProperty({ description: 'e.g. : meta.lastUpdated', required: false })
     _sort: string
 
-  @ApiProperty({ description: 'ex : mot1 AND (mot2 OR mot3)', required: false })
+  @ApiProperty({ description: 'e.g. : mot1 AND (mot2 OR mot3)', required: false })
     _text: string
 
-  @ApiProperty({ description: 'ex : mot1 AND (mot2 OR mot3)', required: false })
+  @ApiProperty({ description: 'e.g. : mot1 AND (mot2 OR mot3)', required: false })
     _content: string
 }
