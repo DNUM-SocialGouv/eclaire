488c7ba0e8a731d5876c041c64df7c2282214821
Fabien <nerocro@gmail.com>
2023-09-01 15:32:18 +0200

    ECL refacto model tests (#105)

--- src/shared/models/data-types/CodingModel.test.ts
+++ src/shared/models/data-types/CodingModel.test.ts
@@ -1,120 +1,49 @@
 import { CodingModel } from './CodingModel'
 import { ModelUtils } from '../eclaire/ModelUtils'
 
-describe('shared | models | fhir | CodingModel', () => {
+describe('shared | models | CodingModel', () => {
   describe('#createResearchStudyPhase', () => {
-    it('should create a properly formatted model with phase when phase 1 is given', () => {
-      expect(CodingModel.createResearchStudyPhase('Human Pharmacology (Phase I)')).toMatchInlineSnapshot(`
-        CodingModel {
-          "code": "phase-1",
-          "display": "Phase 1",
-          "system": "http://terminology.hl7.org/CodeSystem/research-study-phase",
-          "version": "4.0.1",
-        }
-      `)
-    })
+    it.each([
+      ['Human Pharmacology (Phase I)', 'phase-1', 'Phase 1'],
+      ['Therapeutic exploratory (Phase II)', 'phase-2', 'Phase 2'],
+      ['Therapeutic confirmatory (Phase III)', 'phase-3', 'Phase 3'],
+      ['Therapeutic use (Phase IV)', 'phase-4', 'Phase 4'],
+      [ModelUtils.NULL_IN_SOURCE, 'n-a', 'N/A'],
+    ])('should create a properly formatted model with phase when %s is given', (rawPhase, code, display) => {
+      // WHEN
+      const phase = CodingModel.createResearchStudyPhase(rawPhase)
 
-    it('should create a properly formatted model with phase when phase 2 is given', () => {
-      expect(CodingModel.createResearchStudyPhase('Therapeutic exploratory (Phase II)')).toMatchInlineSnapshot(`
-        CodingModel {
-          "code": "phase-2",
-          "display": "Phase 2",
-          "system": "http://terminology.hl7.org/CodeSystem/research-study-phase",
-          "version": "4.0.1",
-        }
-      `)
-    })
-
-    it('should create a properly formatted model with phase 3 when phase is given', () => {
-      expect(CodingModel.createResearchStudyPhase('Therapeutic confirmatory (Phase III)')).toMatchInlineSnapshot(`
-        CodingModel {
-          "code": "phase-3",
-          "display": "Phase 3",
-          "system": "http://terminology.hl7.org/CodeSystem/research-study-phase",
-          "version": "4.0.1",
-        }
-      `)
-    })
-
-    it('should create a properly formatted model with phase when phase 4 is given', () => {
-      expect(CodingModel.createResearchStudyPhase('Therapeutic use (Phase IV)')).toMatchInlineSnapshot(`
-        CodingModel {
-          "code": "phase-4",
-          "display": "Phase 4",
-          "system": "http://terminology.hl7.org/CodeSystem/research-study-phase",
-          "version": "4.0.1",
-        }
-      `)
-    })
-
-    it('should create a properly formatted model without phase when phase is not given', () => {
-      expect(CodingModel.createResearchStudyPhase(ModelUtils.NULL_IN_SOURCE)).toMatchInlineSnapshot(`
-        CodingModel {
-          "code": "n-a",
-          "display": "N/A",
-          "system": "http://terminology.hl7.org/CodeSystem/research-study-phase",
-          "version": "4.0.1",
-        }
-      `)
+      // THEN
+      expect(phase.code).toBe(code)
+      expect(phase.display).toBe(display)
     })
   })
 
   describe('#createGender', () => {
-    it('should create a properly formatted model with female when information is given', () => {
-      expect(CodingModel.createGender('Female')).toMatchInlineSnapshot(`
-        CodingModel {
-          "code": "female",
-          "display": "Female",
-          "system": "http://hl7.org/fhir/administrative-gender",
-          "version": "5.0.0",
-        }
-      `)
-    })
+    it.each([
+      ['Female', 'female'],
+      ['Male', 'male'],
+      ['Unknown', 'unknown'],
+    ])('should create a properly formatted model with %s when information is given', (rawGender, code) => {
+      // WHEN
+      const gender = CodingModel.createGender(rawGender)
 
-    it('should create a properly formatted model with male when information is given', () => {
-      expect(CodingModel.createGender('Male')).toMatchInlineSnapshot(`
-        CodingModel {
-          "code": "male",
-          "display": "Male",
-          "system": "http://hl7.org/fhir/administrative-gender",
-          "version": "5.0.0",
-        }
-      `)
-    })
-
-    it('should create a properly formatted model without gender when information is not given', () => {
-      expect(CodingModel.createGender('unknown')).toMatchInlineSnapshot(`
-        CodingModel {
-          "code": "unknown",
-          "display": "Unknown",
-          "system": "http://hl7.org/fhir/administrative-gender",
-          "version": "5.0.0",
-        }
-      `)
+      // THEN
+      expect(gender.code).toBe(code)
+      expect(gender.display).toBe(rawGender)
     })
   })
 
   describe('#createReglementationPrecision', () => {
-    it('should create a properly formatted model when no low intervention', () => {
-      expect(CodingModel.createReglementationPrecision('No')).toMatchInlineSnapshot(`
-        CodingModel {
-          "code": undefined,
-          "display": "un essai clinique",
-          "system": undefined,
-          "version": undefined,
-        }
-      `)
-    })
+    it.each([
+      ['No', 'un essai clinique'],
+      ['Yes', 'un essai clinique à faible intervention'],
+    ])('should create a properly formatted model when %s is given', (rawPrecision, display) => {
+      // WHEN
+      const reglementationPrecision = CodingModel.createReglementationPrecision(rawPrecision)
 
-    it('should create a properly formatted model when low intervention', () => {
-      expect(CodingModel.createReglementationPrecision('Yes')).toMatchInlineSnapshot(`
-        CodingModel {
-          "code": undefined,
-          "display": "un essai clinique à faible intervention",
-          "system": undefined,
-          "version": undefined,
-        }
-      `)
+      // THEN
+      expect(reglementationPrecision.display).toBe(display)
     })
   })
 })
--- src/shared/models/domain-resources/OrganizationModel.test.ts
+++ src/shared/models/domain-resources/OrganizationModel.test.ts
@@ -1,94 +1,36 @@
 import { OrganizationModel } from './OrganizationModel'
 import { AssignerForSecondaryIdentifier } from '../special-purpose-data-types/ReferenceModel'
 
-describe('organizationModel', () => {
+describe('shared | models | OrganizationModel', () => {
   describe('#createSecondaryAssigner', () => {
     it('should create a properly formatted model when ANSM assigner is given', () => {
-      expect(OrganizationModel.createSecondaryAssigner(AssignerForSecondaryIdentifier.ANSM)).toMatchInlineSnapshot(`
-        OrganizationModel {
-          "address": undefined,
-          "contact": undefined,
-          "id": "ansm",
-          "identifier": undefined,
-          "name": "Agence nationale de sécurité du médicament et des produits de santé",
-          "resourceType": "Organization",
-          "telecom": [
-            ContactPointModel {
-              "extension": undefined,
-              "system": "url",
-              "use": "work",
-              "value": "https://ansm.sante.fr",
-            },
-          ],
-          "type": undefined,
-        }
-      `)
+      // WHEN
+      const organization = OrganizationModel.createSecondaryAssigner(AssignerForSecondaryIdentifier.ANSM)
+
+      // THEN
+      expect(organization.id).toBe('ansm')
+      expect(organization.name).toBe('Agence nationale de sécurité du médicament et des produits de santé')
+      expect(organization.telecom[0].value).toBe('https://ansm.sante.fr')
     })
 
     it('should create a properly formatted model when CTIS assigner is given', () => {
-      expect(OrganizationModel.createSecondaryAssigner(AssignerForSecondaryIdentifier.CTIS)).toMatchInlineSnapshot(`
-        OrganizationModel {
-          "address": undefined,
-          "contact": undefined,
-          "id": "ctis",
-          "identifier": undefined,
-          "name": "Clinical Trials Information System",
-          "resourceType": "Organization",
-          "telecom": [
-            ContactPointModel {
-              "extension": undefined,
-              "system": "url",
-              "use": "work",
-              "value": "https://euclinicaltrials.eu/",
-            },
-          ],
-          "type": undefined,
-        }
-      `)
+      // WHEN
+      const organization = OrganizationModel.createSecondaryAssigner(AssignerForSecondaryIdentifier.CTIS)
+
+      // THEN
+      expect(organization.id).toBe('ctis')
+      expect(organization.name).toBe('Clinical Trials Information System')
+      expect(organization.telecom[0].value).toBe('https://euclinicaltrials.eu/')
     })
 
     it('should create a properly formatted model when EUDRACT assigner is given', () => {
-      expect(OrganizationModel.createSecondaryAssigner(AssignerForSecondaryIdentifier.EUDRACT)).toMatchInlineSnapshot(`
-        OrganizationModel {
-          "address": undefined,
-          "contact": undefined,
-          "id": "eudract",
-          "identifier": undefined,
-          "name": "European Union Drug Regulating Authorities Clinical Trials Database",
-          "resourceType": "Organization",
-          "telecom": [
-            ContactPointModel {
-              "extension": undefined,
-              "system": "url",
-              "use": "work",
-              "value": "https://eudract.ema.europa.eu/",
-            },
-          ],
-          "type": undefined,
-        }
-      `)
-    })
+      // WHEN
+      const organization = OrganizationModel.createSecondaryAssigner(AssignerForSecondaryIdentifier.EUDRACT)
 
-    it('should create a properly formatted model when none assigner is given', () => {
-      expect(OrganizationModel.createSecondaryAssigner(undefined)).toMatchInlineSnapshot(`
-        OrganizationModel {
-          "address": undefined,
-          "contact": undefined,
-          "id": undefined,
-          "identifier": undefined,
-          "name": undefined,
-          "resourceType": "Organization",
-          "telecom": [
-            ContactPointModel {
-              "extension": undefined,
-              "system": "url",
-              "use": "work",
-              "value": undefined,
-            },
-          ],
-          "type": undefined,
-        }
-      `)
+      // THEN
+      expect(organization.id).toBe('eudract')
+      expect(organization.name).toBe('European Union Drug Regulating Authorities Clinical Trials Database')
+      expect(organization.telecom[0].value).toBe('https://eudract.ema.europa.eu/')
     })
   })
 })
