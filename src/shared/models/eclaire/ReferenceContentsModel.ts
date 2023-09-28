82b57b0fa9c008d8405807e66b100e1a77ceecd9
Stéphane Bedeau <46371288+sbedeau@users.noreply.github.com>
2023-09-28 11:57:25 +0200

    refactor: move contained content into referenceContents field (#111)

--- src/shared/models/eclaire/ReferenceContentsModel.ts
+++ src/shared/models/eclaire/ReferenceContentsModel.ts
@@ -1,13 +1,19 @@
-import { Location, Organization } from 'fhir/r4'
+import { Group, Location, Organization } from 'fhir/r4'
 
 export class ReferenceContentsModel {
   private constructor(
+    readonly enrollmentGroup: Group,
     readonly locations: Location[] | undefined,
     readonly organizations: Organization[] | undefined
   ) {}
 
-  static create(locations: Location[], organizations: Organization[]): ReferenceContentsModel {
+  static create(
+    enrollmentGroup: Group,
+    locations: Location[],
+    organizations: Organization[]
+  ): ReferenceContentsModel {
     return new ReferenceContentsModel(
+      enrollmentGroup,
       locations.length === 0 ? undefined : locations,
       organizations
     )
