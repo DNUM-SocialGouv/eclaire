bb643182199e356217161d1bfb9fed8527c5109e
Stéphane Bedeau <stephane.bedeau@octo.com>
2024-03-04 15:33:57 +0100

    feat(ecl-96): add details to swagger description

--- src/api/swagger/swagger.service.ts
+++ src/api/swagger/swagger.service.ts
@@ -6,6 +6,13 @@ export class SwaggerService {
     const builder = new DocumentBuilder()
       .setTitle('API Eclaire')
       .setDescription(`<p>Base nationale des essais cliniques au standard <a href="https://hl7.org/fhir/R4/index.html"><abbr title="Fast Healthcare Interoperability Resources">FHIR</abbr></a>.</p>
+        <p> Les données exposées par l'API ECLAIRE proviennent de sources différentes, toutes disponibles dans le SI RIPH 2G, selon le type d'études cliniques :</p>
+        <ul>
+          <li>Recherches impliquant la personne humaine (1er alinéa de l’article L.1121-1 du code de la santé publique) : « Jardé »</li>
+          <li>Investigations cliniques (article 2 du règlement 2017.745) :  « DM »</li>
+          <li>Etudes de performance  (article 2 du règlement 2017.746) : « DM DIV »</li>
+          <li>Essais cliniques (article 2 du règlement 2014_536) provenant du CTIS et disponibles dans le SI RIPH 2G : « CTIS »</li>
+        </ul>
         <p>Si vous avez des demandes d'améliorations ou des bugs, vous pouvez écrire une <a href="https://github.com/DNUM-SocialGouv/eclaire/issues">issue</a> sur notre Github.</p>
         <p>Pour information :</p>
         <ul>
