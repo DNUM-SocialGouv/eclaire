32116487d00dcabaeec81036590c9828f693854e
Stéphane Bedeau <stephane.bedeau@octo.com>
2025-03-12 17:55:18 +0100

    hotfix: gestion d'erreurs

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import swc from 'unplugin-swc'
 import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  // @ts-ignore
   plugins: [swc.vite({ module: { type: 'es6' } })],
   test: {
     coverage: {
