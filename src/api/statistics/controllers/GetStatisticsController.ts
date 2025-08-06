cbb9b5868c91193d27e770f7dc292f2158e2743c
rami-abdelkader <rami.abdelkader@cgi.com>
2025-08-06 16:04:54 +0200

    add swagger doc to the statistics api

--- src/api/statistics/controllers/GetStatisticsController.ts
+++ src/api/statistics/controllers/GetStatisticsController.ts
@@ -1,13 +1,26 @@
-import { Controller, Get, Inject } from '@nestjs/common'
-import { ApiExcludeController } from '@nestjs/swagger'
+import { Controller, Get, Inject, Header } from '@nestjs/common'
+import {
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiProduces,
+  ApiTags,
+  ApiTooManyRequestsResponse,
+} from '@nestjs/swagger'
 
 import { StatisticsRepository } from '../application/StatisticsRepository'
 
-@ApiExcludeController()
+@ApiTags('Statistics')
 @Controller('/api/statistics')
 export class GetStatisticsController {
   constructor(@Inject('StatisticsRepository') private readonly StatisticsService: StatisticsRepository) {}
 
+  @ApiOperation({ summary: 'Récupère le nombre d’essais cliniques.' })
+  @ApiOkResponse({ description: 'Aperçu des statistiques d’essais cliniques' })
+  @ApiNotFoundResponse({ description: 'Pas de statistiques disponibles' })
+  @ApiTooManyRequestsResponse({ description: 'Trop de requêtes simultanées. Réessayez plus tard.' })
+  @ApiProduces('application/json')
+  @Header('content-type', 'application/json')
   @Get()
   async getStatDocuments(): Promise<Record<string, number>> {
     return await this.StatisticsService.findStat()
