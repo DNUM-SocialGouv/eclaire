8a1367cc870621383a2924e8100ace6b9908720d
rami-abdelkader <rami.abdelkader@cgi.com>
2026-02-27 11:30:57 +0100

    Secure access to export page

--- src/api/file-export/controllers/ExportPageController.ts
+++ src/api/file-export/controllers/ExportPageController.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Res } from '@nestjs/common'
+import { Controller, Get, NotFoundException, Query, Res } from '@nestjs/common'
 import { ApiExcludeController } from '@nestjs/swagger'
 import { Response } from 'express'
 import * as fs from 'fs'
@@ -8,7 +8,12 @@ import * as path from 'path'
 @Controller('/export-page')
 export class ExportPageController {
   @Get()
-  servePage(@Res() res: Response) {
+  servePage( @Query('token') token: string,@Res() res: Response) {
+    // --- Check token ---
+    if (process.env.CRON_TOKEN && token !== process.env.CRON_TOKEN) {
+      throw new NotFoundException()
+    }
+
     const baseDir = path.join(process.cwd(), 'public')
     const filePath = path.join(baseDir, 'export-page.html')
     if (!fs.existsSync(filePath)) return res.status(404).send('Page not found')
